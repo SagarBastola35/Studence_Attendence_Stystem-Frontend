9507dc880c34d87aa9e0bce996e02a88c2c9c0b0
SagarBastola35 <sagarbastolasagar3@gmail.com>
2026-05-26 20:16:39 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.js;
-  
+import App from "./App.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
