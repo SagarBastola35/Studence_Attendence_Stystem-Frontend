7d5695ed9bc4f3148719c928a89f3d9259e04c12
SagarBastola35 <sagarbastolasagar3@gmail.com>
2026-05-26 19:47:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,8 @@ import "./index.css";
 import axios from 'axios';
 import App from "./App.js;
 
-const API_URL = 'https://studence-attendence-stystem-backend-3.onrender.com/api';
+const API_URL = "https://studence-attendence-stystem-backend-3.onrender.com/api";
 axios.defaults.baseURL = API_URL;
-
-axios.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
