b3457fe97756d4c18ccf808008dd4ecc8dda439b
SagarBastola35 <sagarbastolasagar3@gmail.com>
2026-05-26 20:02:19 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import axios from 'axios';
 import App from "./App.js;
   
 createRoot(document.getElementById("root")).render(
