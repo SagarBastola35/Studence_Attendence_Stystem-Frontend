3c33545cf9441f05249d5edad0c4e99787e5d731
SagarBastola35 <sagarbastolasagar3@gmail.com>
2026-05-26 19:54:13 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import axios from 'axios';
 import App from "./App.js;
-
-const API_URL = "https://studence-attendence-stystem-backend-3.onrender.com/api";
-axios.defaults.baseURL = API_URL;
-
+  
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
