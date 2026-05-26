0d5f0c039da0afc0a51f40275bec0c88f96cc86e
SagarBastola35 <sagarbastolasagar3@gmail.com>
2026-05-26 09:53:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -35,14 +35,5 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: false,
     chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          ui: ['framer-motion', 'react-icons', 'recharts'],
-          utils: ['axios', 'react-hot-toast', 'date-fns']
-        }
-      }
-    }
   }
 });
