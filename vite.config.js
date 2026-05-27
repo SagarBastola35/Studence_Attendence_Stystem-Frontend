6269712cf4aa0b8ac18d791ef4a7a8f65af43a39
SagarBastola35 <sagarbastolasagar3@gmail.com>
2026-05-27 18:59:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://studence-attendence-stystem-backend-5.onrender.com",
         changeOrigin: true,
         secure: false,
       },
